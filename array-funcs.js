49e4015609ca3207be706b45cc80629ecf3b840d
Michael Rosata <mrosata@users.noreply.github.com>
2016-09-02 16:23:24 -0400

    Update array-funcs.js

--- array-funcs.js
+++ array-funcs.js
@@ -14,3 +14,10 @@ const capitalize = (s) => splitChars(s)[0].toUpperCase() + s.slice(1);
 const sentenceFromArray = (a) => copyArray(a).join(' ');
 // capitalizeWords :: String -> String
 const capitalizeWords = (s) => sentenceFromArray(forEach(splitWords(s), capitalize));
+
+// TODO: Document below
+const toList = (l) => [].concat(l);
+const tail = (l) => toList(l).slice(1);
+const head = (l) =>  toList(l).slice(0, realNum( toList(l).length - 1) );
+const lastItem = (l) =>  toList(l)[ realNum( toList(l).length -1 ) ];
+const firstItem = (l) => toList(l)[0];
