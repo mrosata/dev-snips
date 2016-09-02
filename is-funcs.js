4ad40242ed2b1ee3ffb8a101c2918bedd1381738
Michael Rosata <mrosata@users.noreply.github.com>
2016-09-02 16:22:32 -0400

    Create is-funcs.js

---
+++ is-funcs.js
@@ -0,0 +1,12 @@
+// isObject :: Mixed -> Boolean
+const isObject = (o) => o && typeof o === "object";
+// isArray :: Mixed -> Boolean
+const isArray = (a) => isObject(a) && a.constructor === Array;
+// isCallable :: Mixed -> Boolean
+const isCallable = (fn) => typeof fn === "function";
+// isUndefined :: Mixed -> Boolean
+const isUndefined = (item) => typeof item === "undefined";
+// isNone :: Mixed -> Boolean
+const isNone = (item) => isUndefined(item) || (!item && typeof item === "object");
+// isEven :: Mixed -> Boolean
+const isEven = (n) => n % 2 === 0;
