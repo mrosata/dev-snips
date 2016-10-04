34233083223601866b80af518b0a93a9f894d9ae
Michael Rosata <mrosata@users.noreply.github.com>
2016-10-04 10:54:23 -0400

    Edited header and added repeat function
    In the future the `repeat` function might go into utilities (just a note)

--- string-funcs.js
+++ string-funcs.js
@@ -1,3 +1,9 @@
+/** 
+ * String Module
+ * @author Michael Rosata
+ * @license MIT
+ */
+
 // testMatch :: RegExp, String -> Boolean
 const testMatch = (regExp, s) => regExp.test(s);
 // firstMatch :: RegExp, String -> Array|null
@@ -6,3 +12,5 @@ const getMatches = (regExp, s) => regExp.exec(s);
 const strReplace = (re, s, replaceWith) => s.replace(re, replaceWith);
 // strReplaceWithFirstMatch :: RegExp, String -> String
 const strReplaceWithFirstMatch = (re, s) => strReplace(re, s, '$1');
+// repeat :: Int, Str -> Str
+const repeat = (amount, str) => Array(amount).fill(str).join('');
