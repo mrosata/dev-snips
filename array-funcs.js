5b541372a87045290436f2365b2687aeb9967c95
Michael Rosata <mrosata@users.noreply.github.com>
2017-10-24 11:46:57 -0400

    now decorates ns

--- array-funcs.js
+++ array-funcs.js
@@ -1,18 +1,35 @@
-// slice :: [a] => [a]
-const slice = [].slice.call.bind([].slice)
-// toList :: (ArrayLike A) => A<a> => [a]
-const toList = as => slice(as)
-// reverse :: [a] -> [a]
-const reverse = (a) => toList(a).reverse()
-// map :: ([a], a -> b) -> [b]
-const map = fn => xs => xs.map(fn)
-// tail :: [a] -> [a]
-const tail = ([x, ...xs]) => xs
-// init :: [a] -> [a]
-const init = xs => xs.length < 2 ? xs : xs.slice(0, xs.length-1)
-// head :: [a] -> [a]
-const head = ([x, ...xs]) => [x]
-// last :: [a] -> a
-const last = ([x, ...xs]) => xs.length === 0 ? x : last(xs)
-// first :: [a] -> a
-const first = ([x, ...xs]) => x
+/**
+ * Drop array utils onto the devSnip namespace
+ *
+ * @param {object} decoratee - the devSnip parent object
+ * @param {string} ns - the devSnip object namespace
+ */
+function arrayFuncs(decoratee = window, ns = 'devSnip') {
+  if (ns) {
+    decoratee[ns] = decoratee[ns] || {}
+  }
+  const devSnips = ns ? decoratee[ns] : decoratee
+  
+  // slice :: [a] => [a]
+  devSnips.slice = [].slice.call.bind([].slice)
+  // toList :: (ArrayLike A) => A<a> => [a]
+  devSnips.toList = as => slice(as)
+  // reverse :: [a] -> [a]
+  devSnips.reverse = (a) => toList(a).reverse()
+  // map :: ([a], a -> b) -> [b]
+  devSnips.map = fn => xs => xs.map(fn)
+  // tail :: [a] -> [a]
+  devSnips.tail = ([x, ...xs]) => xs
+  // init :: [a] -> [a]
+  devSnips.init = xs => xs.length < 2 ? xs : xs.slice(0, xs.length-1)
+  // head :: [a] -> [a]
+  devSnips.head = ([x, ...xs]) => [x]
+  // last :: [a] -> a
+  devSnips.last = ([x, ...xs]) => xs.length === 0 ? x : last(xs)
+  // first :: [a] -> a
+  devSnips.first = ([x, ...xs]) => x
+  
+  return devSnips
+}
+
+arrayFuncs()
